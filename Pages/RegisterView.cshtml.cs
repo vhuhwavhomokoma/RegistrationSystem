4e6aeeb543fc1facefc9db2453d2f5e300bbf07d
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2024-03-19 13:57:41 +0200

    Added RegisterView

---
+++ Pages/RegisterView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RegistrationSystem.Pages
+{
+    public class RegisterViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
