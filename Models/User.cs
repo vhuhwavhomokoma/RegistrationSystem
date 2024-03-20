84cdd6fa1294637405b6b5ffc487ef8c5ccc3e1e
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2024-03-20 16:40:15 +0200

    Implemented User class

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace RegistrationSystem.Models
+{
+    public class User
+    {
+        private string _username;
+        private string _password;
+        public User(string username, string password) { 
+        
+        _username = username;
+        _password = password;
+        
+        }
+
+        public string UserName { get { return _username; } }
+        public string Password { get { return _password; } }
+
+
+    }
+}
