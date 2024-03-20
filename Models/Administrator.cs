3a468f1f0f4e870433b44345fd627fa1ea383b4f
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2024-03-20 16:51:58 +0200

    Implemented Administrator class

---
+++ Models/Administrator.cs
@@ -0,0 +1,15 @@
+﻿namespace RegistrationSystem.Models
+{
+    public class Administrator: User
+    {
+        private int _administratorId;
+
+        public Administrator(string username, string password, int administratorId): base(username, password) 
+            {
+        _administratorId = administratorId;
+        }
+
+        public int AdministratorId { get {  return _administratorId; } }
+
+    }
+}
