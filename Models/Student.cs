4e04b82f202e519ca21c9c4bbdeb5a5812f32d57
vhuhwavhomokoma <vhuhwavho0120@gmail.com>
2024-03-20 16:48:39 +0200

    Implemented Student class

---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace RegistrationSystem.Models
+{
+    public class Student: User
+    {
+        private string _studentId;
+        private string _studentName;
+        private string _course;
+
+        public Student(string username, string password, string studentId, string studentName, string course) : base(username, password)
+        {
+            _studentId = studentId;
+            _studentName = studentName;
+            _course = course;
+
+        }
+
+        public string StudentId { get { return _studentId; } }
+
+        public string StudentName { get { return _studentName; } }
+        
+        public string Course { get { return _course; } }
+
+       
+
+    }
+}
